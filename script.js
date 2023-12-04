367ff167c8fd805288e324a4fe1499d3fa0249af
Abhishek Shivhare <abhishekshivhare@intrakraft.com>
2023-12-04 11:30:39 +0530

    updated

--- script.js
+++ script.js
@@ -7,24 +7,29 @@ set.addEventListener('click', (e) => {
     body.append('company_id', '5f2b8c736047c32705e97d82');
     body.append('user_id', '5f5a1681b117ba0230ab7ba8');
     body.append('otp', otp.value);
-    fetch('https://unicorn-amazing-rhino.ngrok-free.app/api/v2/admin/login/validateOtp', {headers: {'Accept': 'application/json'}, body, method: 'POST', credentials: 'include'})
+    fetch('http://localhost:5002/api/v2/admin/login/validateOtp', {headers: {'Accept': 'application/json'}, body, method: 'POST'})
     .then((response) => {
         console.log(response.headers);
         return response.json()
         .then((responseData) => {
-            console.log(responseData);
-            const cookie = 'session_id=116bb4c3-1404-416f-8627-d905c4341713; Path=/; Expires=Mon, 27 Nov 2023 15:43:58 GMT; HttpOnly';
-            document.cookie = cookie;
+            console.log(responseData.cookie);
+            document.cookie = responseData.cookie;
+            // document.cookie = 'session_id=38568da0-6c82-48a1-81a1-11206c04b0af; Path=/; Expires=Fri, 01 Dec 2023 16:41:01 GMT; Secure';
         });
     }).catch((err) => console.log(err));
     console.log('Set button clicked');
 })
-// get.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     fetch('https://unicorn-amazing-rhino.ngrok-free.app/api/v2/sessionCookie/get', {headers: {'ngrok-skip-browser-warning': 'true', 'Access-Control-Allow-Origin': true, credentials: 'include'}})
-//     .then((response) => {
-//         response.json()
-//         .then((responseData) => console.log(responseData));
-//     }).catch((err) => console.log(err));
-//     console.log('Get button clicked');
-// })
+get.addEventListener('click', (e) => {
+    e.preventDefault();
+    const body = new FormData();
+    body.append('user_id', '5f5a1681b117ba0230ab7ba8')
+    body.append('company_id', '5f2b8c736047c32705e97d82')
+    body.append('user_first_name', 'Abhishek')
+    body.append('user_last_name', 'Shivhare')
+    fetch('https://unicorn-amazing-rhino.ngrok-free.app/api/v2/admin/login/sendOtp', {headers: {'ngrok-skip-browser-warning': 'true', 'Access-Control-Allow-Origin': true}, method: 'POST', body})
+    .then((response) => {
+        response.json()
+        .then((responseData) => console.log(responseData));
+    }).catch((err) => console.log(err));
+    console.log('Get button clicked');
+})
